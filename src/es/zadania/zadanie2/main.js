6eab29e3d257786cdde6712130009bc249532fba
Variw <jako03@wp.pl>
2025-01-15 13:21:11 +0100

    zadanie z asynchronicznosci

--- src/es/zadania/zadanie2/main.js
+++ src/es/zadania/zadanie2/main.js
@@ -1,2 +1,23 @@
 "use strict";
 
+console.log("Downloading data...");
+async function downloadData(index = 1){
+    try{
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${index}`);
+        return await response.json();
+    } catch (error){
+        console.log("ERROR: ", error);
+    }
+}
+const N = 99;
+let allData = []
+for(let i = 1; i <= N; i++){
+    console.clear()
+    console.log(`${(i-1/N * 100).toPrecision(3)}%`);
+    const data = await downloadData(i);
+    allData.push(data);
+}
+console.clear()
+console.log("Data downloaded successfully");
+setTimeout( () => console.log(allData), 2000);
+
