cf2dd1fa751b339ff1fef4bc8f5db9ed9dcaabf9
Variw <jako03@wp.pl>
2025-01-15 16:31:07 +0100

    zadanie z asynchronicznosci

--- src/es/lekcja19/main.js
+++ src/es/lekcja19/main.js
@@ -11,6 +11,7 @@
  */
 
 // 1
+    /*
 const { Worker, isMainThread } = require("worker_threads");
 if(isMainThread){
     console.log("Main thread");
@@ -19,4 +20,22 @@ if(isMainThread){
     console.log("Working thread");
 }
 
+
+     */
 //2
+const cluster = require('cluster');
+const http = require('http');
+const os = require('os');
+
+if(cluster.isMaster) {
+    console.log(`Master process ${process.pid} is running`);
+    for(let i = 0; i < os.cpus().length; i++) {
+        cluster.fork();
+    }
+} else {
+    http.createServer((req, res) => {
+        res.writHead(200);
+        res.end(`Handled by process ${process.pid}`)
+    }).listen(8000);
+    console.log(`Worker ${process.pid} started`);
+}
